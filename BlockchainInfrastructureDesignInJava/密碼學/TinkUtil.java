47246349a7ef28d84d2275f6349ce603e0a57a6f
ChiangWei <albert0425369@gmail.com>
2020-05-04 22:53:58 +0800

    commit tinkutil

--- BlockchainInfrastructureDesignInJava/密碼學/TinkUtil.java
+++ BlockchainInfrastructureDesignInJava/密碼學/TinkUtil.java
@@ -13,6 +13,22 @@ import com.google.crypto.tink.aead.AeadConfig;
 import com.google.crypto.tink.Registry;
 // 自定義初始化
 // import 導入自己的 KeyManager
+// Interface for Authenticated Encryption with Associated Data (AEAD).
+import com.google.crypto.tink.Aead;
+// A KeysetHandle provides abstracted access to Keyset, to limit the exposure of actual protocol buffers that hold sensitive key material.
+import com.google.crypto.tink.KeysetHandle;
+// Static methods for obtaining Aead instances.
+import com.google.crypto.tink.aead.AeadFactory;
+// Pre-generated KeyTemplate for Aead keys.
+import com.google.crypto.tink.aead.AeadKeyTemplates;
+// KeyTemplate
+import com.google.crypto.tink.proto.KeyTemplate;
+// Static methods for reading or writing cleartext keysets.
+import com.google.crypto.tink.CleartextKeysetHandle;
+// A KeysetWriter that can write to some source cleartext or encrypted keysets in proto JSON format.
+import com.google.crypto.tink.JsonKeysetWriter;
+// An abstract representation of file and directory pathnames.
+import java.io.File;
 
 public class TinkUtil {
 	
@@ -37,5 +53,49 @@ public class TinkUtil {
 	// 自定義初始化
 	public void register03() {
 		// Registry.registerKeyManager(new KeyManager());
+	}
+	
+	// AEAD 加密解密
+	// associated data to be authenticated
+	public void aead(byte[] plaintext, byte[] associated) {
+		try {
+			// 1. 配置生成密鑰集
+			KeysetHandle keysetHandle = KeysetHandle.generateNew(AeadKeyTemplates.AES128_GCM);
+			// 2. 使用 Key 獲取所選實例
+			Aead aead = AeadFactory.getPrimitive(keysetHandle);
+			// 3. 完成加密
+			byte[] ciphertext = aead.encrypt(plaintext, associated);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 生成包含隨機生成的 AES 128 - GCM 密鑰的密鑰集
+	public void createKeySet() {
+		try {
+			// pre-generated templates
+			KeyTemplate keyTemplate = AeadKeyTemplates.AES128_GCM;
+			KeysetHandle keysetHandle = KeysetHandle.generateNew(keyTemplate);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 生成密鑰後，將其保存到儲存系統中，以寫入文件為例
+	public void save2File() {
+		try {
+			// 創建 AES 對應的 keysetHandle
+			KeysetHandle keysetHandle = KeysetHandle.generateNew(AeadKeyTemplates.AES128_GCM);
+			// 寫入 json 文件
+			String keysetFilename = "my_keyset.json";
+			CleartextKeysetHandle.write(keysetHandle, JsonKeysetWriter.withFile(new File(keysetFilename)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 使用 Google Cloud KMS key 來對 key 加密
+	public void save2FileBaseKMS() {
+		// gcp-kms://projects/tink-examples/locations/global/keyRings/foo/cryptoKeys/bar
 	}
 }
