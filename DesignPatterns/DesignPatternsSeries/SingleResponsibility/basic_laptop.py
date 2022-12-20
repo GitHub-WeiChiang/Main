a50fd468c7d6ec7c7c7f863df2aef9f2608fb153
ChiangWei <albert0425369@gmail.com>
2022-12-20 17:30:22 +0800

    go

---
+++ DesignPatterns/DesignPatternsSeries/SingleResponsibility/basic_laptop.py
@@ -0,0 +1,21 @@
+class Laptop: 
+    cpu = ""
+    size = 0
+    
+    def startUp(self):
+        pass
+    
+    def shutDown(self): 
+        pass
+
+class BasicLaptop(Laptop):
+    cpu = ""
+    size = 0
+    
+    def startUp(self):
+        pass
+    
+    def shutDown(self):
+        pass
+
+BasicLaptop()
