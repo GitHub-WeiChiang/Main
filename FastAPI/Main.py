14f9ca801f2cab91ed085bb73fa1236793cc2f98
ChiangWei <albert0425369@gmail.com>
2022-12-25 21:19:08 +0800

    fastapi 完成第一步, 增加問題紀錄

--- FastAPI/Main.py
+++ FastAPI/Main.py
@@ -5,9 +5,12 @@ from fastapi import FastAPI
 from Router import router
 from starlette.responses import RedirectResponse
 
+# FastAPI 是直接从 Starlette 继承的类
+# Starlette is a lightweight ASGI framework / toolkit, which is ideal for building async web services in Python.
 app = FastAPI()
 
 
+# 路径操作装饰器
 @app.get("/")
 async def root():
     return RedirectResponse("http://127.0.0.1:8000/docs#/")
--- Questions/README.md
+++ Questions/README.md
@@ -2,6 +2,8 @@ Questions
 =====
 * ### Question001 Timing Attack
 * ### Question002 Bloom Filter
+* ### Question003 什麼是 WSGI ?
+* ### Question004 什麼是 ASGI ?
 <br />
 
 Reference
